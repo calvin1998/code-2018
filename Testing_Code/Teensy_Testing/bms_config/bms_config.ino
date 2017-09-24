7fb01783ae5e47674af3478f563f6b232f1589dc
Ryan Gallaway <rgallaway@gatech.edu>
2017-09-24 18:47:40 -0400

    wrote simple BMS configuration program

---
+++ Testing_Code/Teensy_Testing/bms_config/bms_config.ino
@@ -0,0 +1,64 @@
+#include <FlexCAN.h>
+#include <Metro.h>
+#include <HyTech17.h>
+
+FlexCAN CAN(500000);
+CAN_message_t msg;
+String command_incoming;
+bool command_finished = false;
+
+void setup() {
+  Serial.begin(115200); //initialize serial for PC communication
+  CAN.begin();
+  delay(200);
+  Serial.println("CAN transceiver initialized");
+  Serial.println("Enter <address>,<value>");
+}
+
+uint32_t t = 1;
+
+void loop() {
+  while (CAN.read(msg)) {
+    if (msg.id == ID_BMS_CONFIG) {
+      BMS_config bms_config = BMS_config(msg.buf);
+      Serial.print("Updated address ");
+      Serial.print(bms_config.getAddress());
+      Serial.print(" to value ");
+      Serial.println(bms_config.getValue());
+    }
+  }
+
+  /*
+   * Get user input
+   */
+  if (Serial.available() > 0) {
+    char newChar = (char) Serial.read();
+    if (newChar != '\n') {
+      command_incoming += newChar;
+    } else {
+      command_finished = true;
+    }
+  }
+
+  if (command_finished) {
+    command_finished = false;
+    char *params[2]; // address,value
+    char *cmd = const_cast<char*> (command_incoming.c_str());
+    params[0] = strtok(cmd, ",");
+    int i = 0;
+    while (params[i] != NULL) {
+        params[++i] = strtok(NULL, ",");
+    }
+
+    uint8_t address = atoi(params[0]);
+    short value = atoi(params[1]);
+    BMS_config bms_config = BMS_config();
+    bms_config.setAddress(address);
+    bms_config.setValue(value);
+    bms_config.write(msg.buf);
+    msg.id = ID_BMS_CONFIG;
+    msg.len = 2;
+    CAN.write(msg);
+    Serial.println("Sent to BMS");
+  }
+}
