81c708189a0f3bf151d39380e3750afb9e0bfbf9
alexanderfache <alexander.fache@gmail.com>
2017-09-26 20:21:35 -0400

    Starting pseudocode

---
+++ Testing_Code/Live_Data_Visualization/LiveDataPlotting.py
@@ -0,0 +1,29 @@
+
+
+//read serial port from Teensy
+
+[][] data //store incoming data, voltage/temp/current
+
+//figure setup
+create figure
+axis, limits, etc
+
+while available
+    read serial port
+    parse package and add to data if new received
+        -compare with "key" characters to get correct variable type
+
+    if (new data point was added) {
+        update plot
+    } //not necessary
+
+end
+
+
+function plot([] newDataThatWasAddedSinceLastPlot)
+//array... of new data added since last plot
+update plot
+if
+
+
+
