62f38d16fa5828b33aad1f5c78d9ff5e571abbe0
Karvin Dassanayake <karvin9@gmail.com>
2017-09-28 23:46:48 -0400

    Sending 2 variables for test code

--- Testing_Code/Live_Data_Visualization/Arduino_Test_Code/Arduino_Test_Code.ino
+++ Testing_Code/Live_Data_Visualization/Arduino_Test_Code/Arduino_Test_Code.ino
@@ -1,14 +1,21 @@
+int counter = 0;
+
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(9600);
     delay(100);
+    counter = 0;
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
     for(int i = 0; i < 100; i++) {
-        Serial.println(i);
+        // print to serial
         delay(100);
+        Serial.print(counter++);
+        Serial.print(" ");
+        Serial.print(i);
+        Serial.print("\n");
     }
 
 }
