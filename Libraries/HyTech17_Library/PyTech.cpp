aa5230aa224d0a9e9ad14695c2e1d4c46213202f
Karvin Dassanayake <karvin9@gmail.com>
2017-10-10 14:00:43 -0400

    BMS_currents implemented

--- Libraries/HyTech17_Library/PyTech.cpp
+++ Libraries/HyTech17_Library/PyTech.cpp
@@ -8,7 +8,7 @@ pybind11_setup(cfg)
 
 #include "HyTech17.h"
 #include "BMS_status.cpp"
-// #include <BMS_currents.cpp>
+#include "BMS_currents.cpp"
 
 #include <pybind11/pybind11.h>
 // #include <pybind11/stl.h>
@@ -17,12 +17,14 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(PyTech, m)
 {
+    // NOTE: uint8_t array constructor doesn't compile, and write and load functionality is ...misunderstood(?) - look at https://trello.com/c/IumyUtGR
+
     // BMS_status
     py::class_<BMS_status>(m, "BMS_status")
         .def(py::init<>())
         // .def(py::init<std::uint8_t[]>())
-        .def("load", &BMS_status::load)
-        .def("write", &BMS_status::write)
+        // .def("load", &BMS_status::load)
+        // .def("write", &BMS_status::write)
         .def("getOvervoltage", &BMS_status::getOvervoltage)
         .def("getUndervoltage", &BMS_status::getUndervoltage)
         .def("getTotalVoltageHigh", &BMS_status::getTotalVoltageHigh)
@@ -40,15 +42,22 @@ PYBIND11_MODULE(PyTech, m)
         .def("setChargeOvertemp", &BMS_status::setChargeOvertemp)
         .def("setBMSStatusOK", &BMS_status::setBMSStatusOK);
 
-    // // BMS Current
-    // py::class_<BMS_currents> currents(m, "BMS_currents");
-    // currents.def(py::init<>());
-    // currents.def(py::init<uint8_t[]>());
-    // currents.def(py::init<float, uint8_t);
-    // currents.def("load", &BMS_currents::load);
-    // currents.def("write", &BMS_currents::write);
-    // currents.def("getCurrent", &BMS_currents::getCurrent);
-    // currents.def("getChargingState", &BMS_currents::getChargingState);
-    // currents.def("setCurrent", &BMS_currents::setCurrent);
-    // currents.def("setChargingState", &BMS_currents::setChargingState);
+    // BMS_currents
+    py::class_<BMS_currents>(m, "BMS_currents")
+        .def(py::init<>())
+        // .def(py::init<uint8_t[]>())
+        // .def(py::init<float, uint8_t)
+        // .def("load", &BMS_currents::load)
+        // .def("write", &BMS_currents::write)
+        .def("getCurrent", &BMS_currents::getCurrent)
+        .def("getChargingState", &BMS_currents::getChargingState)
+        .def("setCurrent", &BMS_currents::setCurrent)
+        .def("setChargingState", &BMS_currents::setChargingState);
+    
+    // BMS_temperatures
+    // BMS_voltages
+    
+    // TODO: MC messages
+    // TODO: PCU messages
+
 }
