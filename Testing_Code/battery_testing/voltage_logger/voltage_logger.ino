7a7138bd0cf6ddab820a6441d2e87c0e7252ad4c
robert_kuramshin <robert.kuramshin98@gmail.com>
2017-11-30 19:16:10 -0500

    more precise measurements and better outputs

--- Testing_Code/battery_testing/voltage_logger/voltage_logger.ino
+++ Testing_Code/battery_testing/voltage_logger/voltage_logger.ino
@@ -6,8 +6,11 @@
 #define TPIN 9
 #define VPIN A0
 
+double start_voltage = 0;
 double cell_voltage = 0;
 double last_cell_voltage = 0;
+double voltage_drop = 0;
+double internal_resistance=0;
 
 unsigned long start = millis();
 unsigned long last_millis = 0;
@@ -28,7 +31,7 @@ void setup() {
 void loop() {
   double voltage_reading = analogRead(VPIN) * (V_REF / 1023.0);
   double current_millis = millis();
-  cell_voltage = (voltage_reading/R2)*(R1+R2)*(3.724/3.71);
+  cell_voltage = (voltage_reading/R2)*(R1+R2)*(3.342/3.329);
 
   current = cell_voltage/RLOAD;
 
@@ -41,36 +44,55 @@ void loop() {
     start = millis();
     analogWrite(9,255);
     discharging = true;
+    voltage_drop = (start_voltage-cell_voltage);
+    internal_resistance = voltage_drop/current;
     Serial.println("----------------!!!Discharging!!!----------------");
+    Serial.print("Voltage Drop: ");
+    Serial.println(voltage_drop);
+  }else if(!discharging&&!end_discharge){
+    start_voltage = cell_voltage;
+    analogWrite(9,255);
   }
 
   if(discharging){
-   
     double period = current_millis - last_millis;
     charge += current*1000 * (period/1000/60/60);    
     Serial.print("Elapsed Time: ");
     Serial.print((current_millis-start)/1000);
     Serial.print("s Voltage: ");
-    Serial.print(cell_voltage);
-    Serial.print("V Current: ");
-    Serial.print(current);
-    Serial.print("A Elapsed Charge: ");
+    Serial.print(1000*cell_voltage);
+    Serial.print("mV Current: ");
+    Serial.print(1000*current);
+    Serial.print("mA Elapsed Charge: ");
     Serial.print(charge);
-    Serial.println("mA");
+    Serial.println("mAh");
   }else if(end_discharge){
     current = 0;
     Serial.print("Elapsed Time: ");
     Serial.print((current_millis-start)/1000);
-    Serial.print("s Voltage: ");
-    Serial.print(cell_voltage);
-    Serial.print("V Current: ");
-    Serial.print(current);
-    Serial.print("A Elapsed Charge: ");
+    
+    Serial.print("s Ellapsed Charge: ");
     Serial.print(charge);
-    Serial.println("mA");
+
+    Serial.print("mAh Start Voltage: ");
+    Serial.print(1000*start_voltage);
+    
+    Serial.print("mV Voltage: ");
+    Serial.print(1000*cell_voltage);
+ 
+    Serial.print("mV Current: ");
+    Serial.print(1000*current);
+    
+    Serial.print("mA Voltage Drop: ");
+    Serial.print(1000*voltage_drop);
+    
+    Serial.print("mV Internal Resistance: ");
+    Serial.print(1000000*internal_resistance);
+    Serial.println("uohm");
   }else{
     Serial.print("Voltage: ");
-    Serial.println(cell_voltage);
+    Serial.print(1000*cell_voltage);
+    Serial.println("mV");
   }
 
 
